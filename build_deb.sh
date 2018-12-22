27a5695afc44067e597750260f12b9fed5ea7558
Tom <twitchyliquid64@ciphersink.net>
2018-12-21 19:18:13 -0800

    Add manpage

--- build_deb.sh
+++ build_deb.sh
@@ -20,4 +20,9 @@ go build -o "${BUILD_DIR}/usr/bin/debdep" github.com/twitchyliquid64/debdep/debd
 mkdir -pv "${BUILD_DIR}/DEBIAN"
 cp -rv ${SCRIPT_BASE_DIR}/DEBIAN/* "${BUILD_DIR}/DEBIAN"
 
+
+mkdir -pv "${BUILD_DIR}/usr/share/man/man1"
+cp "${SCRIPT_BASE_DIR}/debdep.1" "${BUILD_DIR}/usr/share/man/man1/debdep.1"
+gzip "${BUILD_DIR}/usr/share/man/man1/debdep.1"
+
 dpkg-deb --build "${BUILD_DIR}" ./
